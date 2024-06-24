01882ba957719a743759220392728fe4a148da77
leoagea <leoagea@gmail.com>
2024-06-24 19:08:56 +0200

    Add : add token funct

--- src/lexer/tokens.c
+++ src/lexer/tokens.c
@@ -6,13 +6,27 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 13:21:14 by lagea             #+#    #+#             */
-/*   Updated: 2024/06/24 16:17:32 by lagea            ###   ########.fr       */
+/*   Updated: 2024/06/24 19:08:38 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-int test(void)
+int add_token(t_tokens *token)
 {
-	printf("test\n\n");
+	t_tokens *new;
+
+	new = malloc(sizeof(t_tokens));
+	if (!new)
+		return 1;
+	new->str = NULL;
+	token->next = new;
+	token = token->next;
+	token->next = NULL;
+	return 0;
+}
+
+int token_read(char *input, t_tokens *token)
+{
+
 }
