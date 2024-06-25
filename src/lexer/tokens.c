901d6f40eb5a1d65057ec349748965062e63b0a5
leoagea <leoagea@gmail.com>
2024-06-25 18:58:26 +0200

    Done on 25/06

--- src/lexer/tokens.c
+++ src/lexer/tokens.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tokens.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
+/*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 13:21:14 by lagea             #+#    #+#             */
-/*   Updated: 2024/06/24 21:29:19 by lagea            ###   ########.fr       */
+/*   Updated: 2024/06/25 16:34:37 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,5 +54,22 @@ int read_without_quote(char *input, t_tokens *token)
 
 int	token_read(char *input, t_tokens *token)
 {
-	read_without_quote(input, token);
+	int i;
+	char **split;
+	t_tokens *head;
+
+	head = token;
+	i = 0;
+	split = ft_split_minishell(input, '|');
+	while (split[i] != NULL)
+	{
+		printf("chaine splite, i : %d , str : %s\n", i, split[i]);
+		// read_without_quote(split[i], token);
+		i++;
+	}
 }
+//echo | cat > infile -e | pwd
+
+//flag in quot
+
+//tant que tu ne voie pas un | > < >> << un wihtespacec c est le meme token
--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 16:09:13 by lagea             #+#    #+#             */
-/*   Updated: 2024/06/24 17:04:57 by lagea            ###   ########.fr       */
+/*   Updated: 2024/06/25 18:31:23 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,10 @@ int main(void){
     // If the input is not NULL, print it and free the allocated memory
     if (input) {
         // printf("You entered: %s\n", input);
-		token_read(input, &token);
+		// token_read(input, &token);
+        // int j = quote_border(input, 0, '"');
+        // printf("j : %d\n", j);
+        test_2(input, &token);
         free(input);
     }
 	return 0;
