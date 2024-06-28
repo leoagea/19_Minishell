b5385f205f0c64770d9299fd04bceef4e05fc30a
Leo Agea <leoagea@gmail.com>
2024-06-28 21:37:38 +0200

    Fix : Norme

--- src/lexer/utils.c
+++ src/lexer/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
+/*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 21:23:33 by lagea             #+#    #+#             */
-/*   Updated: 2024/06/28 17:56:29 by lagea            ###   ########.fr       */
+/*   Updated: 2024/06/28 21:37:27 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	check_whitespace(char *str, int i)
 	return (str[i] == ' ' || (str[i] >= (int)9 && str[i] <= 13));
 }
 
-int check_special_char(char *str, int i){
-	return ((str[i] == '>' && str[i + 1] == '>') || (str[i] == '<' && str[i + 1] == '<') || str[i] == '>' || str[i] == '<' || str[i] == '|');
+int	check_special_char(char *str, int i)
+{
+	return ((str[i] == '>' && str[i + 1] == '>') || (str[i] == '<' && str[i
+			+ 1] == '<') || str[i] == '>' || str[i] == '<' || str[i] == '|');
 }
