ab689308d63e932d55c2f389f5bec22283367caf
Leo Agea <leoagea@gmail.com>
2024-06-28 21:28:47 +0200

    Update : clean minishell.c

--- src/minishell.c
+++ src/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
+/*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 16:09:13 by lagea             #+#    #+#             */
-/*   Updated: 2024/06/28 17:00:43 by lagea            ###   ########.fr       */
+/*   Updated: 2024/06/28 21:26:53 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,29 +25,14 @@ int main(void){
 
         // If the input is not NULL, print it and free the allocated memory
         if (input) {
-            // printf("You entered: %s\n", input);
+            
             str = ft_strdup(input);
             add_history(str);
-            // printf("%d\n", check_whitespace(str, 0));
-            // printf("old str : '%s'\n",str);
-            
-            // int i = 0;
-            // while (check_whitespace(str, i))
-            //     i++;
-            // char *skip = ft_substr(str, 0 , i);
-            // str = ft_strtrim(str, skip);
-
-            // printf("new str : '%s'\n",str);
             
             lexer(str, &token);
-            
-            // int j = quote_border(input, 0, '"');
-            // printf("j : %d\n", j);
+
             free(input);
-            // if (str){
             free(str);
-            //     str = NULL;   
-            // }
         }
     }
 	return 0;
