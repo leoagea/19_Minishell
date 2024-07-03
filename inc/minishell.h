0aa9b66567407a1fb88a74890b86b3a49808a626
leoagea <leoagea@gmail.com>
2024-07-03 15:14:11 +0200

    Add : add cmd struct for parser

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/22 20:10:37 by vdarras           #+#    #+#             */
-/*   Updated: 2024/07/03 13:22:28 by lagea            ###   ########.fr       */
+/*   Updated: 2024/07/03 15:13:59 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,31 +30,15 @@ typedef enum s_bool
 	TRUE   //  1
 }					t_bool;
 
-// typedef enum s_type
-// {
-// 	INPUT = 1, // '<'                              0
-// 	TRUNC,     // '>'                              1
-// 	HEREDOC,   // '<<'                             2
-// 	APPEND,    // '>>'                             3
-// 	PIPE,      // '|'                              4
-// 	CMD,       // 'COMMAND'  (cat, ls, ...)        5
-// 	OPTION,    // 'OPTION'   (-n, -la, ...)        6
-// 	ARG        // 'ARGUMENT DE COMMANDE' (Ce qu'il y a apres une commande comme echo par exemple,entre quotes ou non) 7
-// }					t_type;
-
-typedef struct s_tokens
+typedef struct s_cmd
 {
-	t_type type;    // C'est le type de token entre 0,1,2,...,6 enumere au dessus
-	char *str;       // C'est la chaine de caractere literale
-	struct s_tokens	*next;
-}					t_tokens;
-
-typedef struct s_command
-{
-	int 			infile;    		// fd infile
-	int 			outfile;    	// fd outifile
-	char			**command; 	// complete command : array of string to passs in execve
-}					t_command;
+    char                    *str; //cmd, arg, flag
+    // int                        (builtin)(t_data , struct s_cmd);
+    int                        num_redirections;
+    t_node                    *redirections;
+    struct s_cmd    *next;
+    struct s_cmd    *prev;
+}                t_cmd;
 
 /*--------------------------------LEXER----------------------------------*/
 /*-------------------------------Tokens----------------------------------*/
