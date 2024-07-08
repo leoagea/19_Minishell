ca121f39528962eaea84c505221b59d087281f09
leoagea <leoagea@gmail.com>
2024-07-08 19:39:04 +0200

    Add : sweep_word function

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -6,27 +6,55 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 15:18:44 by lagea             #+#    #+#             */
-/*   Updated: 2024/07/05 17:43:54 by lagea            ###   ########.fr       */
+/*   Updated: 2024/07/08 19:16:53 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+
+static void sweep_word(t_data *data, char *str, t_node *current)
+{
+	int i;
+	char cpy;
+	char *expand;
+	i = 0;
+	data->env_expand = env_var_init();
+	while (str[i])
+	{
+		// if (str[i] == 34)
+		// 	expand = handle_double_quotes(str, i);
+		// else if (str [i] == 39)
+		// 	expand = handle_single_quotes(str, i);
+		if (str[i] == '$')
+		{
+			printf("1\n");
+			expand = handle_env_variables(data, str, i);
+		}
+		i++;
+	}
+}
+
+//avancer dans le word , si on rrentre dans auncuin if copie str[i] dans la cpy
+// modifie le i par pointeur
+// si $ on expand la variable de la bonne taille
+// puis on join sur la cpy
 
 int expander(t_data *data)
 {
 	char *str;
 	t_node *current;
 	
-	data->expander = dll_init();
+	data->expander = dll_init(); 
 	current = data->lexer->head;
 	while (current != NULL)
 	{
 		str = current->str;
-		if (ft_strncmp(str, ">>", 2) == 0 || ft_strncmp(str, "<<", 2) == 0 || ft_strncmp(str, ">", 1) == 0 || ft_strncmp(str, "<", 1) == 0 || ft_strncmp(str, "|", 1) == 0)
+		if (current->type != PIPE && current->type != INPUT && current->type != TRUNC && current->type != HEREDOC && current->type != APPEND)
 		{
-			printf("test\n");
+			printf("test ");
+			printf("%s\n", str);
+			sweep_word(data, str, current);
 		}
-		// if (check_expand_error_code(current))
 		current = current->next;
 	}
 	return 0;
