eb403f4bce6f05822a7409ab41382050045c0871
leoagea <leoagea@gmail.com>
2024-07-12 21:45:06 +0200

    Update : print env corrected

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -4,12 +4,17 @@ void	print_env(t_lst *env)
 {
 	t_env	*node;
 
+	if (!env)
+		return ;
 	node = env->head;
 	while (node != NULL)
 	{
-		ft_printf("%s=",node->var);
 		if (node->flag == 1)
-			ft_printf("%s\n", node->value);
+		{
+			ft_printf("%s=",node->var);
+			if (node->value)
+				ft_printf("=%s\n", node->value);
+		}
 		node = node->next;
 	}
 }
