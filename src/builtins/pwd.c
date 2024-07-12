3466a883728d6bcb047a6a9d276c7ec7a84860df
leoagea <leoagea@gmail.com>
2024-07-12 21:44:37 +0200

    Update : correct pwd and get_pwd

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -7,30 +7,34 @@ char *get_pwd(void)
     char *pwd;
     char buffer[BUFFER_SIZE];
 
-    if (!getcwd(buffer, BUFFER_SIZE))
-        return (NULL);
-    len = ft_strlen(buffer);
-    pwd = malloc(sizeof(char) * len + 1);
+    pwd = getcwd(NULL, 0);
     if (!pwd)
         return NULL;
-    i = -1;
-    while(buffer[i])
-        pwd[i] = buffer[i];
+    // if (!(getcwd(buffer, BUFFER_SIZE)))
+    //     return NULL;
+    // len = ft_strlen(buffer);
+    // pwd = malloc(sizeof(char) * len + 1);
+    // printf("pwd : %s\n", pwd);
+    // if (!pwd)
+    //     return NULL;
+    // i = -1;
+    // while(buffer[i])
+    //     pwd[i] = buffer[i];
     return (pwd);
 }
 
 int pwd(void)
 {
-    char buffer[BUFFER_SIZE];
+    char *pwd;
 
-    if (!getcwd(buffer, BUFFER_SIZE))
+    if (!getcwd(pwd, BUFFER_SIZE))
     {
         ft_putstr_fd("Error :", 2);
         ft_putstr_fd(strerror(errno), 2);
         ft_putchar_fd('\n', 2);
         return (1);
     }
-    ft_putstr_fd(buffer, 1);
+    ft_putstr_fd(pwd, 1);
     ft_putchar_fd('\n', 1);
     return (0);
 }
