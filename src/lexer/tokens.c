3e163e84ca7dff106accea0166fccff485450554
leoagea <leoagea@gmail.com>
2024-07-16 16:08:53 +0200

    Fix : Norme OK

--- src/lexer/tokens.c
+++ src/lexer/tokens.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokens.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 15:56:36 by lagea             #+#    #+#             */
+/*   Updated: 2024/07/16 16:06:04 by lagea            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../inc/minishell.h"
 
-static int in_quote(char *str, int i)
+static int	in_quote(char *str, int i)
 {
-	char quote;
+	char	quote;
 
 	quote = str[i];
 	i++;
@@ -17,469 +29,84 @@ static int in_quote(char *str, int i)
 			quote = str[i];
 			i++;
 		}
-		else if ((check_special_char(str, i) || check_whitespace(str, i)) && str[i])
-			return i;
+		else if ((check_special_char(str, i) || check_whitespace(str, i))
+			&& str[i])
+			return (i);
 	}
-	return i;
-
+	return (i);
 }
 
-static void assign_type(t_dll *tokens)
+static void	assign_type(t_dll *tokens)
 {
-	t_node *current;
+	t_node	*current;
 
 	current = tokens->head;
 	while (current != NULL)
 	{
 		if (ft_strncmp(current->str, ">>", 2) == 0)
-			current->type = 4;	
+			current->type = 4;
 		else if (ft_strncmp(current->str, "<<", 2) == 0)
 			current->type = 3;
 		else if (ft_strncmp(current->str, ">", 1) == 0)
 			current->type = 2;
 		else if (ft_strncmp(current->str, "<", 1) == 0)
-			current->type = 1;	
+			current->type = 1;
 		else if (ft_strncmp(current->str, "|", 1) == 0)
 			current->type = 5;
 		else if (ft_strncmp(current->str, "-", 1) == 0)
 			current->type = 7;
-		else 
+		else
 			current->type = 6;
 		current = current->next;
 	}
 }
 
-
-int lexer(char *input, t_dll *tokens)
+static void	not_in_quote(char *input, int *i, int *start)
 {
-	int i = 0;
-	int start;
-	char *word;
+	if (input[*i])
+	{
+		*i = skip_whitespace(input, *i);
+		*start = *i;
+		if (check_special_char(input, *i))
+			while (check_special_char(input, *i))
+				*i += 1;
+		else
+		{
+			while (!check_whitespace(input, *i) && !check_special_char(input,
+					*i) && input[*i])
+			{
+				if ((input[*i] == (int)34 || input[*i] == (int)39) && input[*i])
+					*i = in_quote(input, *i);
+				else
+					*i += 1;
+			}
+		}
+	}
+}
 
+int	lexer(char *input, t_dll *tokens)
+{
+	int		i;
+	int		start;
+	char	*word;
+
+	i = 0;
 	while (input[i])
 	{
 		i = skip_whitespace(input, i);
-		if((input[i] == (int)34 || input[i] == (int) 39) && input[i])
+		if ((input[i] == (int)34 || input[i] == (int)39) && input[i])
 		{
 			start = i;
 			i = in_quote(input, i);
 		}
 		else
-		{
-			if (input[i])
-			{
-				i = skip_whitespace(input, i);
-				start = i;
-				if (check_special_char(input, i))
-					while (check_special_char(input, i))
-						i++;
-				else
-				{
-					while (!check_whitespace(input, i) && !check_special_char(input, i) && input[i])
-					{
-						if((input[i] == (int)34 || input[i] == (int) 39) && input[i])
-							i = in_quote(input, i);
-						else
-							i++;
-					}
-				}
-			}
-		}
+			not_in_quote(input, &i, &start);
 		word = ft_substr(input, start, i - start);
 		if (check_open_quote(word) == 1)
-			return (write(1, "Error: open quote\n",18), 1);
+			return (write(1, "Error: open quote\n", 18), 1);
 		dll_insert_tail(word, tokens);
 		i = skip_whitespace(input, i);
 	}
-	// dll_print_forward(tokens);
 	assign_type(tokens);
-	if (check_open_pipe(tokens))
-		return (write(1, "Error: open pipe\n",17), exit(1), 1);
-	else if (check_open_redirect(tokens))
-		return (write(1, "Error: open redirection\n",24), exit(1), 1);
-	else if (check_wrong_token(tokens))
-		return (write(1, "Error: wrong token\n",19), exit(1), 1);
-	// dll_print_forward(tokens);
-	return 0;
+	return (do_all_check(tokens));
 }
-
-
-
-// "te""st" "cat" | infile > ls
-
-// "'''hola'''"'""hello""'
-
-// echo test"de la mort"te tete
-
-// "Hello"'>''<''|'|"Hello"
-
-/*
-minishell$ "HOLA"
-test
-start : 0
-i : 6
-word : "HOLA"
-
-start : 7
-i : 8
-word : 
-
-minishell$ "'HOLA'"
-test
-start : 0
-i : 8
-word : "'HOLA'"
-
-minishell$ "''HOLA"
-test
-start : 0
-i : 8
-word : "''HOLA"
-
-minishell$ "'''''HOLA'''''HELOO'''"
-test
-start : 0
-i : 24
-word : "'''''HOLA'''''HELOO'''"
-
-minishell$ Hello ca
-start : 0
-i : 5
-word : Hello
-
-start : 6
-i : 8
-word : ca
-
-start : 9
-i : 14
-word : 
-
-start : 15
-i : 16
-word : 
-
-start : 17
-i : 18
-word : 
-
-minishell$ Hello cat 
-start : 0
-i : 5
-word : Hello
-
-start : 6
-i : 9
-word : cat
-
-minishell$      
-start : 0
-i : 5
-word :      
-
-minishell$            
-start : 0
-i : 11
-word :            
-
-minishell$     
-start : 0
-i : 4
-word :     
-
-minishell$    
-start : 0
-i : 3
-word :    
-
-minishell$     
-start : 0
-i : 4
-word :     
-
-minishell$ "     "
-test
-start : 0
-i : 7
-word : "     "
-
-minishell$ '     '
-test
-start : 0
-i : 7
-word : '     '
-
-minishell$ "Hello"'>''<''|'|"Hello"
-test
-start : 0
-i : 8
-word : "Hello"'
-
-test
-start : 9
-i : 11
-word : ''
-
-test
-start : 12
-i : 14
-word : ''
-
-test
-start : 15
-i : 24
-word : '|"Hello"
-
-start : 25
-i : 30
-word : 
-
-minishell$ "Hello"">""<""|"|"Hello"
-test
-start : 0
-i : 8
-word : "Hello""
-
-test
-start : 9
-i : 11
-word : ""
-
-test
-start : 12
-i : 14
-word : ""
-
-test
-start : 15
-i : 24
-word : "|"Hello"
-
-minishell$ "hello'>"
-test
-start : 0
-i : 7
-word : "hello'
-
-test
-start : 8
-i : 9
-word : "
-*/
-
-
-/*----------------------------------*/
-/*----------------------------------*/
-/*----------------------------------*/
-/*----------------------------------*/
-/*----------------------------------*/
-/*----------------------------------*/
-/*----------------------------------*/
-/*----------------------------------*/
-
-/*
-
-minishell$ echo lol
-start : 0
-i : 4
-word : echo
-
-start : 5
-i : 8
-word : lol
-
-minishell$ echo << test
-start : 0
-i : 4
-word : echo
-
-start : 5
-i : 6
-word : <
-
-start : 6
-i : 7
-word : <
-
-start : 8
-i : 12
-word : test
-
-minishell$ echo >> er
-start : 0
-i : 4
-word : echo
-
-start : 5
-i : 6
-word : >
-
-start : 6
-i : 7
-word : >
-
-start : 8
-i : 10
-word : er
-
-minishell$ echo test || cat lo
-start : 0
-i : 4
-word : echo
-
-start : 5
-i : 9
-word : test
-
-start : 10
-i : 11
-word : |
-
-start : 11
-i : 12
-word : |
-
-start : 13
-i : 16
-word : cat
-
-start : 17
-i : 19
-word : lo
-
-minishell$ echo lol <<< test
-start : 0
-i : 4
-word : echo
-
-start : 5
-i : 8
-word : lol
-
-start : 9
-i : 10
-word : <
-
-start : 10
-i : 11
-word : <
-
-start : 11
-i : 12
-word : <
-
-start : 13
-i : 17
-word : test
-
-minishell$ echo "test de la mort" 
-start : 0
-i : 4
-word : echo
-
-start : 5
-i : 22
-word : "test de la mort"
-
-minishell$ echo test"de la mort"te tete
-start : 0
-i : 4
-word : echo
-
-start : 5
-i : 12
-word : test"de
-
-start : 13
-i : 15
-word : la
-
-start : 16
-i : 23
-word : mort"te
-
-start : 24
-i : 28
-word : tete
-
-minishell$ echo "ere er er e"
-start : 0
-i : 4
-word : echo
-
-start : 5
-i : 18
-word : "ere er er e"
-
-minishell$ echo test'de lamort'erwr
-start : 0
-i : 4
-word : echo
-
-start : 5
-i : 12
-word : test'de
-
-start : 13
-i : 24
-word : lamort'erwr
-
-minishell$ echo "test ' de la" mort '
-start : 0
-i : 4
-word : echo
-
-start : 5
-i : 19
-word : "test ' de la"
-
-start : 20
-i : 24
-word : mort
-
-start : 25
-i : 26
-word : '
-
-minishell$ echo test << test << test << 
-start : 0
-i : 4
-word : echo
-
-start : 5
-i : 9
-word : test
-
-start : 10
-i : 11
-word : <
-
-start : 11
-i : 12
-word : <
-
-start : 13
-i : 17
-word : test
-
-start : 18
-i : 19
-word : <
-
-start : 19
-i : 20
-word : <
-
-start : 21
-i : 25
-word : test
-
-start : 26
-i : 27
-word : <
-
-start : 27
-i : 28
-word : <
-
-minishell$ */
--- src/lexer/utils.c
+++ src/lexer/utils.c
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 21:23:33 by lagea             #+#    #+#             */
-/*   Updated: 2024/07/02 17:01:25 by lagea            ###   ########.fr       */
+/*   Updated: 2024/07/16 16:08:32 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,13 @@ int	check_whitespace(char *str, int i)
 int	check_special_char(char *str, int i)
 {
 	return ((str[i] == '>' && str[i + 1] == '>') || (str[i] == '<' && str[i
-			+ 1] == '<') || str[i] == '>' || str[i] == '<' || str[i] == '|');
+				+ 1] == '<') || str[i] == '>' || str[i]
+		== '<' || str[i] == '|');
 }
 
-int skip_whitespace(char *str, int i)
+int	skip_whitespace(char *str, int i)
 {
 	while (check_whitespace(str, i) && str[i])
 		i++;
-	return i;
+	return (i);
 }
