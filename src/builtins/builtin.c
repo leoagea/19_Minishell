039383a36eb5c9e93120196e7bd3b9d77cd9f4cc
vdarras <vdarras27@gmail.com>
2024-08-01 19:57:35 +0200

    Builtins fixed without pipeline

--- src/builtins/builtin.c
+++ src/builtins/builtin.c
@@ -6,7 +6,7 @@
 /*   By: vdarras <vdarras@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 17:21:10 by lagea             #+#    #+#             */
-/*   Updated: 2024/08/01 17:30:03 by vdarras          ###   ########.fr       */
+/*   Updated: 2024/08/01 19:46:29 by vdarras          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,12 +77,21 @@ void	exec_builtin_no_pipe(t_cmd *command, t_data *data)
 
 int		check_simple_builtin(t_cmd *node, t_data *data)
 {
-	if (node->next == NULL && node->is_builtin == true && ft_strncmp(node->str[0], "exit", INT_MAX) != 0)
+	if (node->next == NULL && node->is_builtin == true && ft_strncmp(node->str[0], "exit", INT_MAX) != 0
+														&& ft_strncmp(node->str[0], "export", INT_MAX) != 0
+														 && ft_strncmp(node->str[0], "unset", INT_MAX) != 0
+														  && ft_strncmp(node->str[0], "cd", INT_MAX) != 0)
 	{
 		exec_builtin_no_pipe(node, data);
 		return (1);
 	}
-	else if (ft_strncmp(node->str[0], "exit", INT_MAX) == 0)
+	else if (node->next == NULL && node->is_builtin == true && ft_strncmp(node->str[0], "exit", INT_MAX) == 0)
 		__exit(data, node);
+	else if (node->next == NULL && node->is_builtin == true && ft_strncmp(node->str[0], "export", INT_MAX) == 0)
+		_export(data, node);
+	else if (node->next == NULL && node->is_builtin == true && ft_strncmp(node->str[0], "unset", INT_MAX) == 0)
+		unset(data, node);
+	else if (node->next == NULL && node->is_builtin == true && ft_strncmp(node->str[0], "cd", INT_MAX) == 0)
+		cd(data);
 	return (0);
 }
