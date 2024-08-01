2b17e9f28f9af28e8363ad04682ddda36b0eb8d5
leoagea <leoagea@gmail.com>
2024-08-01 17:26:03 +0200

    Fix : Norme

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -6,21 +6,21 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 15:18:44 by lagea             #+#    #+#             */
-/*   Updated: 2024/07/31 14:35:58 by lagea            ###   ########.fr       */
+/*   Updated: 2024/08/01 15:32:15 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-void	heredoc_token(t_data *data, t_node *current, char *cpy)
+void	heredoc_token(t_data *data, t_node *current, char *str)
 {
-	dll_insert_tail(ft_strdup(cpy), data->expander);
+	dll_insert_tail(ft_strdup(str), data->expander);
 	data->expander->tail->type = current->type;
 	current = current->next;
-	cpy = expand_heredoc(current->str);
-	dll_insert_tail(ft_strdup(cpy), data->expander);
+	str = expand_heredoc(current->str);
+	dll_insert_tail(ft_strdup(str), data->expander);
 	data->expander->tail->type = current->type;
-}	
+}
 
 static void	other_token(t_data *data, t_node *current)
 {
@@ -28,8 +28,10 @@ static void	other_token(t_data *data, t_node *current)
 	data->expander->tail->type = current->type;
 }
 
-static void	word(t_data *data, t_node *current, char *cpy, char *str)
+static void	word(t_data *data, t_node *current, char *str)
 {
+	char	*cpy;
+
 	cpy = sweep_word(data, str);
 	if (!cpy || cpy[0] == '\0')
 		return ;
@@ -38,14 +40,11 @@ static void	word(t_data *data, t_node *current, char *cpy, char *str)
 	data->expander->tail->type = current->type;
 }
 
-int	expander(t_data *data)
+static void	dispatch_expand(t_data *data)
 {
 	char	*str;
-	char	*cpy;
 	t_node	*current;
 
-	data->expander = dll_init();
-	data->env_expand = env_var_init();
 	current = data->lexer->head;
 	while (current != NULL)
 	{
@@ -53,19 +52,30 @@ int	expander(t_data *data)
 		if (current->type != PIPE && current->type != INPUT
 			&& current->type != TRUNC && current->type != APPEND
 			&& current->type != HEREDOC)
-				word(data, current, cpy, str);
+			word(data, current, str);
 		else if (current->type == HEREDOC)
 		{
-			heredoc_token(data, current, cpy);
+			heredoc_token(data, current, str);
 			current = current->next;
 		}
 		else
 			other_token(data, current);
 		current = current->next;
 	}
+}
+
+int	expander(t_data *data)
+{
+	char	*cpy;
+
+	data->expander = dll_init();
+	if (!data->expander)
+		return (1);
+	data->env_expand = env_var_init();
+	if (!data->env_expand)
+		return (1);
+	dispatch_expand(data);
 	check_node_null(data);
-	// free_exp(data->env_expand);
-	// free_str(data->env_expand->var);
 	free(data->env_expand);
 	data->env_expand = NULL;
 	return (0);
