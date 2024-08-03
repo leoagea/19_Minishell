5092a2200f6a1329b51b9461ac912f3385b81555
Leo Agea <leoagea@gmail.com>
2024-08-04 00:47:32 +0200

    Fix : Norme

--- src/exec/error_management.c
+++ src/exec/error_management.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error_management.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vdarras <vdarras@student.42.fr>            +#+  +:+       +#+        */
+/*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 14:17:19 by vdarras           #+#    #+#             */
-/*   Updated: 2024/08/02 17:24:09 by vdarras          ###   ########.fr       */
+/*   Updated: 2024/08/04 00:46:40 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ void	no_access(char *command)
 	if (stat(command, &data) == -1)
 	{
 		perror("stat");
-		exit (1);
+		exit(1);
 	}
 	if ((data.st_mode & S_IFMT) == S_IFDIR)
 	{
 		write(2, "bash: ", 6);
 		write(2, command, ft_strlen(command));
 		write(2, ": is a directory\n", ft_strlen(": is a directory\n"));
-		exit (126);
+		exit(126);
 	}
 }
 
@@ -34,8 +34,8 @@ void	command_not_found(char *command)
 {
 	write(2, "bash: ", 6);
 	write(2, command, ft_strlen(command));
-	write(2,  ": command not found\n", ft_strlen(": command not found\n"));
-	exit (127);
+	write(2, ": command not found\n", ft_strlen(": command not found\n"));
+	exit(127);
 }
 
 void	error_management(char *command)
@@ -48,8 +48,9 @@ void	error_management(char *command)
 		{
 			write(2, "bash: ", 6);
 			write(2, command, ft_strlen(command));
-			write(2,  ": No such file or directory\n", ft_strlen(": No such file or directory\n"));
-			exit (127);
+			write(2, ": No such file or directory\n",
+				ft_strlen(": No such file or directory\n"));
+			exit(127);
 		}
 	}
 	command_not_found(command);
